1a772fdc794c639619f6352995fc33bff1b77189
Daniel Landim <danielc.landim@gmail.com>
2025-09-19 17:51:08 -0300

    ajustando para adicionar o SwaggerModule :hammer:

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Packing API')
+    .setDescription('API para calcular caixas necessárias para pedidos')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+  console.log('Listening on http://localhost:3000');
+  console.log('Swagger UI: http://localhost:3000/api');
 }
 bootstrap();
